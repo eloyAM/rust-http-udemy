22010413bcce6d2f852fd848da34f2490571b2da
Eloy AM <eloy.abellanm@um.es>
2021-08-09 01:19:31 +0200

    L28 enums - returning to normal enum

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,4 @@
 fn main() {
-    let get = Method::GET("Hello".to_string());
-    let delete = Method::DELETE(69);
-    let post = Method::POST;
-    // Not possible casting
-    // an `as` expression can only be used to convert between primitive types or to coerce to a specific trait object
-    dbg!(get as String); dbg!(delete as u64); dbg!(post as u64);
-    
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -33,8 +26,8 @@ struct Request {
 }
 
 enum Method {
-    GET(String),
-    DELETE(u64),
+    GET,
+    DELETE,
     POST,
     PUT,
     HEAD,

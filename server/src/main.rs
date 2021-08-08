ad96809f8ea78435f95cdf0bae986baa6b002bde
Eloy AM <eloy.abellanm@um.es>
2021-08-09 01:08:12 +0200

    L28 enum - method enum

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    dbg!(get as u64); dbg!(delete as u64); dbg!(post as u64);
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -17,4 +21,22 @@ impl Server {
     fn run (self) {
         println!("Listening on {}", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
 }

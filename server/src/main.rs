3b8ea39cf5c9dd040b3eccab0f2699069d05a77b
Eloy AM <eloy.abellanm@um.es>
2021-08-09 01:45:07 +0200

    L30 modules - folder for submodules

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,33 +2,9 @@ use server::Server;
 use http::request::Request;
 
 mod server;
+mod http;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-mod http {
-    pub mod request {
-        use super::method::Method;
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method
-        }
-    }
-
-    pub mod method {
-        pub enum Method {
-            GET,
-            DELETE,
-            POST,
-            PUT,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH
-        }
-    }
 }

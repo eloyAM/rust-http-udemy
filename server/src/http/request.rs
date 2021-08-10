36f738cfe3c29399c9e9cc2521f34c48f7377169
Eloy AM <eloy.abellanm@um.es>
2021-08-11 00:37:35 +0200

    L38 traits - concept behind

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,6 +1,14 @@
 use super::method::Method;
+use std::convert::TryFrom;
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method
+}
+
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!()
+    }
 }
